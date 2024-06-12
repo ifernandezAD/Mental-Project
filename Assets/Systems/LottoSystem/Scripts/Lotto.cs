92ccadfad2c5cbddfbf1bfb9ea85da4b954cf6c6
ifernandezAD <88330098+ifernandezAD@users.noreply.github.com>
2024-06-12 12:29:55 +0200

    Started working on loto pool

--- Assets/Systems/LottoSystem/Scripts/Lotto.cs
+++ Assets/Systems/LottoSystem/Scripts/Lotto.cs
@@ -4,13 +4,83 @@ using UnityEngine;
 
 public class Loto : MonoBehaviour
 {
-    [SerializeField] private Happiness happiness;
+    [Header("Testing")]
+    [SerializeField] bool testPrintLotoPool;
+    [SerializeField] bool testSubstractFromPool;
+    [SerializeField] bool testRefillPool;
 
+    [SerializeField] CharacterController characterController;
     [SerializeField] private int maxValue;
     private List<int> lottoPool;
 
-    void Start()
+    void OnValidate()
     {
+        if (testPrintLotoPool)
+        {
+            TestingPrintLottoPool();
+            testPrintLotoPool = false;
+        }
+
+        if (testSubstractFromPool)
+        {
+            SubtractFromPool();
+            testSubstractFromPool = false;
+        }
+
+        if (testRefillPool)
+        {
+            RefillPool();
+            testRefillPool = false;
+        }
 
     }
+
+    void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Start()
+    {
+        maxValue = characterController.selectedCharacter.happinessRange;
+        lottoPool = new List<int>();
+
+        for (int i = 1; i <= maxValue; i++)
+        {
+            lottoPool.Add(i);
+        }
+    }
+
+    public void SubtractFromPool()
+    {
+        if (lottoPool.Count == 0)
+        {
+            Debug.LogWarning("The lotto pool is empty.");
+            RefillPool();
+            return;
+        }
+
+        int randomIndex = Random.Range(0, lottoPool.Count);
+        int randomValue = lottoPool[randomIndex];
+        lottoPool.RemoveAt(randomIndex);
+        Debug.Log("Value " + randomValue + " removed from the pool.");
+    }
+
+    void RefillPool()
+    {
+        lottoPool.Clear();
+        for (int i = 1; i <= maxValue; i++)
+        {
+            lottoPool.Add(i);
+        }
+    }
+
+    void TestingPrintLottoPool()
+    {
+        foreach (int value in lottoPool)
+        {
+            Debug.Log(value);
+        }
+    }
+
 }

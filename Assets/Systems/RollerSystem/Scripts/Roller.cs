5cc29c7da02ec0181d2fd1ce723daa873dc58818
ifernandezAD <88330098+ifernandezAD@users.noreply.github.com>
2024-06-18 13:02:53 +0200

    Roller Updated
    You can add and remove slots

--- Assets/Systems/RollerSystem/Scripts/Roller.cs
+++ Assets/Systems/RollerSystem/Scripts/Roller.cs
@@ -2,15 +2,61 @@ using UnityEngine;
 
 public class Roller : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Testing")]
+    [SerializeField] bool testAddSlot;
+    [SerializeField] bool testRemoveSlot;
+
+    [Header("Variables")]
+    [SerializeField] GameObject slots; 
+    [SerializeField] int activeSlotCount = 3; 
+
+void OnValidate()
+{
+    if(testAddSlot)
     {
-        
+        AddSlot();
+        testAddSlot = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    if(testRemoveSlot)
     {
-        
+        RemoveSlot();
+        testRemoveSlot = false;
+    }
+}
+
+ void Start()
+    {
+        int slotIndex = 0;
+        foreach (Transform slot in slots.transform)
+        {
+            if (slotIndex < activeSlotCount)
+            {
+                slot.gameObject.SetActive(true);
+            }
+            else
+            {
+                slot.gameObject.SetActive(false);
+            }
+            slotIndex++;
+        }
+    }
+
+    public void AddSlot()
+    {
+        if (activeSlotCount < slots.transform.childCount)
+        {
+            slots.transform.GetChild(activeSlotCount).gameObject.SetActive(true);
+            activeSlotCount++;
+        }
+    }
+
+    public void RemoveSlot()
+    {
+        if (activeSlotCount > 0)
+        {
+            activeSlotCount--;
+            slots.transform.GetChild(activeSlotCount).gameObject.SetActive(false);
+        }
     }
 }

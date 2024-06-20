f74fd8cce736588beb61eaa5acf93364da89543f
ifernandezAD <88330098+ifernandezAD@users.noreply.github.com>
2024-06-20 11:33:22 +0200

    Quit Game Enabled

---
+++ Assets/{Utilities}/QuitGame/Scripts/QuitGame.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class QuitGame : MonoBehaviour
+{
+    [SerializeField] InputActionReference escapeGame;
+
+    private void OnEnable() { escapeGame.action.Enable(); }
+
+    private void Update() { UpdateEscapeGame(); }
+
+    private void UpdateEscapeGame()
+    {
+        if (escapeGame.action.WasPerformedThisFrame())
+        {
+            Application.Quit();
+        }
+    }
+    private void OnDisable() { escapeGame.action.Disable(); }
+
+
+
+
+}

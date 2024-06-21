536714190f37ab93df3d6fb380cbb0c2701bf196
ifernandezAD <88330098+ifernandezAD@users.noreply.github.com>
2024-06-21 11:29:48 +0200

    Testing Card Architecture done

--- Assets/Cards/Scripts/EnemyCardDisplay.cs
+++ Assets/Cards/Scripts/EnemyCardDisplay.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using System;
 
 
 public class EnemyCardDisplay : MonoBehaviour
@@ -7,10 +9,17 @@ public class EnemyCardDisplay : MonoBehaviour
  [SerializeField] EnemyCard enemyCard;
 
  [SerializeField] Image cardArt;
+ [SerializeField] TextMeshProUGUI lifeText;
+ [SerializeField] TextMeshProUGUI attackText;
+ [SerializeField] TextMeshProUGUI skillText;
 
  void Start()
  {
     cardArt.sprite = enemyCard.art;
+
+    lifeText.text = "Life: " + enemyCard.health;
+    attackText.text = enemyCard.attack.ToString();
+    skillText.text = enemyCard.skill;
  }
 
 }

ae9e4bf52d44e6dd4094fe9cb76863e0e57fb05b
ifernandezAD <88330098+ifernandezAD@users.noreply.github.com>
2024-06-29 18:13:43 +0200

    Card Methods trigger over bubble release

--- Assets/Systems/DragAndDropSystem/Scripts/DraggableButton.cs
+++ Assets/Systems/DragAndDropSystem/Scripts/DraggableButton.cs
@@ -1,11 +1,14 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class DraggableButton : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
 {
-    private bool isDragging = false;
+    private bool isReleased = false;
     private RectTransform rectTransform;
     private Canvas canvas;
+
+    private CardDetector cardDetector;
 
     void Start()
     {
@@ -15,47 +18,41 @@ public class DraggableButton : MonoBehaviour, IPointerDownHandler, IDragHandler,
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        isDragging = true;
+        isReleased=false;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        if (isDragging)
-        {
+
             Vector2 localPoint;
-            // Use the appropriate camera for the canvas
             Camera camera = canvas.renderMode == RenderMode.ScreenSpaceOverlay ? null : canvas.worldCamera;
 
-            // Convert the screen point to local point in the canvas
             RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.transform as RectTransform, eventData.position, camera, out localPoint);
 
-            // Convert the local point in the canvas to world point
             Vector2 worldPoint = canvas.transform.TransformPoint(localPoint);
 
-            // Convert the world point to local point in the parent of the button
             Vector2 localPointInParent = rectTransform.parent.InverseTransformPoint(worldPoint);
 
-            // Assign the new local position to the button
             rectTransform.localPosition = localPointInParent;
-        }
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
-        isDragging = false;
+        isReleased=true;
     }
+
+    
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        CardDetector cardDetector = other.GetComponent<CardDetector>();
-        if (cardDetector != null)
+         cardDetector = other.GetComponent<CardDetector>();   
+    }
+
+   private void OnTriggerStay2D()
+   {
+        if(isReleased)
         {
             cardDetector.HandleButtonRelease(this);
         }
-    }
-
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        // Optionally handle trigger exit events if needed
-    }
+   }
 }

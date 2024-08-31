44fb676230fe466c6be2e933f6e7fedf244fb9e0
ifernandezAD <88330098+ifernandezAD@users.noreply.github.com>
2024-08-31 10:50:43 +0200

    Refactor bubbles and ? bubble working

--- Assets/Systems/RollerSystem/Scripts/Phases/PlayerPhase.cs
+++ Assets/Systems/RollerSystem/Scripts/Phases/PlayerPhase.cs
@@ -9,14 +9,8 @@ public class PlayerPhase : Phase
     [SerializeField] Button okButton;
 
     [Header("Bubbles")]
-    [SerializeField] GameObject attackBubble;
-    [SerializeField] GameObject resilienceBubble;
-    [SerializeField] GameObject staminaBubble;
+    [SerializeField] GameObject[] bubbles;
     [SerializeField] Transform bubbleContainer;
-
-    private List<GameObject> attackBubbles = new List<GameObject>();
-    private List<GameObject> resilienceBubbles = new List<GameObject>();
-    private List<GameObject> staminaBubbles = new List<GameObject>();
 
     protected override void InternalOnEnable()
     {
@@ -34,50 +28,47 @@ public class PlayerPhase : Phase
     private void InstantiateBubbles()
     {
         Roller roller = Roller.instance;
-        roller.UpdateImageCount(); 
+        roller.UpdateImageCount();
 
         int swordCount = roller.GetImageCount(ImageType.Sword);
         int heartCount = roller.GetImageCount(ImageType.Heart);
         int bookCount = roller.GetImageCount(ImageType.Book);
+        int poisonCount = roller.GetImageCount(ImageType.Poison);
+        int randomCount = roller.GetImageCount(ImageType.Random);
 
         for (int i = 0; i < swordCount; i++)
         {
-            GameObject bubble = Instantiate(attackBubble, bubbleContainer);
-            attackBubbles.Add(bubble);
+            GameObject bubble = Instantiate(bubbles[0], bubbleContainer);
         }
 
         for (int i = 0; i < heartCount; i++)
         {
-            GameObject bubble = Instantiate(resilienceBubble, bubbleContainer);
-            resilienceBubbles.Add(bubble);
+            GameObject bubble = Instantiate(bubbles[1], bubbleContainer);
         }
 
         for (int i = 0; i < bookCount; i++)
         {
-            GameObject bubble = Instantiate(staminaBubble, bubbleContainer);
-            staminaBubbles.Add(bubble);
+            GameObject bubble = Instantiate(bubbles[2], bubbleContainer);
+        }
+
+        for (int i = 0; i < poisonCount; i++)
+        {
+            GameObject bubble = Instantiate(bubbles[3], bubbleContainer);
+        }
+
+        for (int i = 0; i < randomCount; i++)
+        {
+            int randomIndex = UnityEngine.Random.Range(0, bubbles.Length);
+            GameObject bubble = Instantiate(bubbles[randomIndex], bubbleContainer);
         }
     }
 
     private void DestroyAllBubbles()
     {
-        foreach (GameObject bubble in attackBubbles)
+        foreach (Transform bubble in bubbleContainer)
         {
-            Destroy(bubble);
+            Destroy(bubble.gameObject);
         }
-        attackBubbles.Clear();
-
-        foreach (GameObject bubble in resilienceBubbles)
-        {
-            Destroy(bubble);
-        }
-        resilienceBubbles.Clear();
-
-        foreach (GameObject bubble in staminaBubbles)
-        {
-            Destroy(bubble);
-        }
-        staminaBubbles.Clear();
     }
 
     void ResetResilience()

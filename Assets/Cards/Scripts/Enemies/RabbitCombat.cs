6846c55eb990efea57629cef3950db4457d8eb27
ifernandezAD <88330098+ifernandezAD@users.noreply.github.com>
2024-09-12 10:02:33 +0200

    Rabbit Working

--- Assets/Cards/Scripts/Enemies/RabbitCombat.cs
+++ Assets/Cards/Scripts/Enemies/RabbitCombat.cs
@@ -5,11 +5,11 @@ public class RabbitCombat : CombatBehaviour
     [Range(0, 100)]
     public float dodgeChance = 25f;
 
-    public override void Attack()
+    public override void Defense(int damage)
     {
         float randomValue = Random.Range(0f, 100f);
         if (randomValue <= dodgeChance) { return; }
 
-        base.Attack();
+        base.Defense(damage);
     }
 }

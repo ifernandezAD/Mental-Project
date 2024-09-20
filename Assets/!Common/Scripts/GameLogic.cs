9110909019da821a19a21ef192d87f1f9b7d7ae3
ifernandezAD <88330098+ifernandezAD@users.noreply.github.com>
2024-09-20 12:47:49 +0200

    Updated GameLogic Script

--- Assets/!Common/Scripts/GameLogic.cs
+++ Assets/!Common/Scripts/GameLogic.cs
@@ -13,15 +13,9 @@ public class GameLogic : MonoBehaviour
     [SerializeField] public GameObject[] availableCharacters; 
     [SerializeField] public Transform bubblesContainer;
 
-    [Header("Ally Related Variables")]
-    [SerializeField] public Transform allyContainer;
-    [SerializeField] GameObject[] allyArray;
-    private List<GameObject> remainingAllies;
-
     void Awake()
     {
         instance = this;
-        remainingAllies = new List<GameObject>(allyArray);
     }
 
     void Start()
@@ -29,30 +23,5 @@ public class GameLogic : MonoBehaviour
         int selectedCharacterIndex = PlayerPrefs.GetInt("SelectedCharacter", 0); 
         GameObject selectedCharacter = Instantiate(availableCharacters[selectedCharacterIndex], characterContainer);
         mainCharacterCard = selectedCharacter;
-    }
-
-    void OnEnable()
-    {
-        //AllyEvent.onAllyObtained += AddAlly;
-    }
-
-    private void AddAlly(int allyIndex)
-    {
-        if (allyIndex >= 0 && allyIndex < remainingAllies.Count)
-        {
-            GameObject selectedAlly = remainingAllies[allyIndex];
-            GameObject newAlly = Instantiate(selectedAlly, allyContainer);
-
-            remainingAllies.RemoveAt(allyIndex);
-        }
-        else
-        {
-            Debug.LogWarning("Índice de aliado fuera de rango o inválido.");
-        }
-    }
-
-    void OnDisable()
-    {
-        //AllyEvent.onAllyObtained -= AddAlly;
     }
 }

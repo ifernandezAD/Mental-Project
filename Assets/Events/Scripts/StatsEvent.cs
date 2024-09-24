3a08a02252d7639675dc115c1dabff599b5582de
ifernandezAD <88330098+ifernandezAD@users.noreply.github.com>
2024-09-24 13:00:44 +0200

    Stats Event Done

---
+++ Assets/Events/Scripts/StatsEvent.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class StatsEvent : MonoBehaviour
+{
+    [Header("References")]
+    private GameObject characterCard;
+    private Health health;
+    private Skill skill;
+
+    [Header("Variables")]
+    [SerializeField] int healthAmount = 2;
+    [SerializeField] int resilienceAmount = 2;
+    [SerializeField] int staminaAmount = 3;
+
+    void Start()
+    {
+        characterCard = GameLogic.instance.mainCharacterCard;
+        health = characterCard.GetComponent<Health>();
+        skill = characterCard.GetComponent<Skill>();
+    }
+
+    public void AddHealthEvent()
+    {
+        health.AddHealth(healthAmount);
+    }
+
+    public void AddResilienceEvent()
+    {
+        health.AddResilience(resilienceAmount);
+    }
+
+    public void AddStaminaEvent()
+    {
+        skill.IncreaseStamina(staminaAmount);
+    }
+}

d52860122dcbe20a9aabf1a453384c76db9c140f
ifernandezAD <88330098+ifernandezAD@users.noreply.github.com>
2025-02-09 15:09:48 +0100

    Bird Character Added
    Skill and mental effects pending

---
+++ Assets/MentalHealthEffects/Scripts/BirdMentalHealthEffects.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class BirdMentalHealthEffects : MentalHealthEffects
+{
+    public override void TriggerFirstActMentalHealthEffects()
+    {
+        //Establecer 25% de Flashback
+    }
+
+    public override void TriggerSecondActMentalHealthEffects()
+    {
+        //Establecer 50% de Flashback
+    }
+
+    public override void TriggerThirdActMentalHealthEffects()
+    {
+        //Establecer 75% de Flashback
+    }
+}
---
+++ Assets/Skills/Scripts/Character/DebuffAttackSkill.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class DebuffAttackSkill : Skill
+{
+    public override void TriggerSkill()
+    {
+        //Implementar la debuff skill
+    }
+}

975664405ab9a79c6acf06195af2693be04ea697
ifernandezAD <88330098+ifernandezAD@users.noreply.github.com>
2025-02-14 15:50:34 +0100

    Chance of good or bad flashback added

--- Assets/Events/Scripts/GenericEvent.cs
+++ Assets/Events/Scripts/GenericEvent.cs
@@ -3,10 +3,20 @@ using UnityEngine;
 public class GenericEvent : MonoBehaviour
 {
     public bool IsFlashback { get; private set; }
+    public bool IsGoodFlashback { get; private set; } 
 
     public void Initialize(bool isFlashback)
     {
         IsFlashback = isFlashback;
-        Debug.Log($"Evento inicializado. Es flashback: {IsFlashback}");
+
+        if (IsFlashback)
+        {
+            IsGoodFlashback = Random.value < 0.5f; 
+            Debug.Log($"Evento de flashback inicializado. Es flashback: {IsFlashback}, Es bueno: {IsGoodFlashback}");
+        }
+        else
+        {
+            Debug.Log($"Evento normal inicializado.");
+        }
     }
 }
